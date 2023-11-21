4c3ab3a380846eca78246f2f059d18df6bab16de
G00401535@atu.ie <G00401535@atu.ie>
2023-11-21 14:14:35 +0000

    code to add content to file

---
+++ src/main/java/org/example/WriteToFile.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+public class WriteToFile {
+
+    public static void main(String[] args) {
+        String fileName = "File14.txt";
+        try (FileWriter writer = new FileWriter(fileName)) {
+
+            writer.write("some content for the file.");
+
+            System.out.println("content successfully written to file.");
+        }
+        catch (IOException e) {
+            System.out.println("An error occurrd while writing to file.");
+            e.printStackTrace();
+        }
+    }
+}
